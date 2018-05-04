dd8ca3b5cb4d22715fa17c464a347263554891ce
Benjamin <sabean.ben@gmail.com>
2018-05-04 12:06:34 -0300

    Can retrieve SSID and password from the user.

--- ThinkTankESP/ThinkTankESP.ino
+++ ThinkTankESP/ThinkTankESP.ino
@@ -27,24 +27,24 @@ ESP8266WebServer server(80);
 
 //login page, also called for disconnect
 void handleSubmit() {
-  Serial.println("GET: Submit"); 
   String content = "<html><body><H2>WiFi information updated!</H2><br>";
   server.send(200, "text/html", content);
   digitalWrite(STATUS_LIGHT, LOW);
   WiFi.mode(WIFI_STA);
+  waitForConnect();
   apMode = false;
 }
 
 //root page can be accessed only if authentification is ok
 void handleRoot() {
-  Serial.println("GET: Root"); 
   String msg;
   if (server.hasArg("SSID") && server.hasArg("PASSWORD")) {
     Serial.println("SSID: " + server.arg("SSID"));
     Serial.println("Password: " + server.arg("PASSWORD"));
     server.sendContent("HTTP/1.1 301 OK\r\nLocation: /success\r\nCache-Control: no-cache\r\n\r\n");
+    return;
   }
-  String content = "<html><body><form action='/success' method='POST'>Please enter new SSID and password.<br>";
+  String content = "<html><body><form action='/' method='POST'>Please enter new SSID and password.<br>";
   content += "SSID:<input type='text' name='SSID' placeholder='SSID'><br>";
   content += "Password:<input type='password' name='PASSWORD' placeholder='password'><br>";
   content += "<input type='submit' name='SUBMIT' value='Submit'></form>" + msg + "<br>";
@@ -52,7 +52,6 @@ void handleRoot() {
 }
 
 void handleNotFound() {
-  Serial.println("GET: Unknown resource"); 
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -71,17 +70,53 @@ void btnHandler() {
   digitalWrite(STATUS_LIGHT, HIGH);
   Serial.println("\nButton pressed!");
 
-  // AP SERVER
-  Serial.print("Setting soft-AP ... ");
-  WiFi.mode(WIFI_AP_STA);
-  Serial.println(WiFi.softAP(AP_SSID) ? "Ready" : "Failed");
-  Serial.println("");
+  if (!apMode) {
+    // AP SERVER
+    Serial.print("Setting soft-AP ... ");
+    WiFi.mode(WIFI_AP_STA);
+    if (WiFi.softAP(AP_SSID)) {
+      Serial.println("Ready");
+    }
+    else {
+      Serial.println("Failed");
+      return;
+    }
+    Serial.println("");
 
-  // Wait for connection
+    // Wait for connection
+    waitForConnect();
+
+    server.on("/", handleRoot);
+    server.on("/success", handleSubmit);
+    server.on("/inline", []() {
+      server.send(200, "text/plain", "this works without need of authentification");
+    });
+
+    server.onNotFound(handleNotFound);
+    //here the list of headers to be recorded
+    const char * headerkeys[] = {
+      "User-Agent", "Cookie"
+    } ;
+    size_t headerkeyssize = sizeof(headerkeys) / sizeof(char*);
+    //ask server to track these headers
+    server.collectHeaders(headerkeys, headerkeyssize );
+    server.begin();
+    Serial.println("HTTP server started");
+
+    apMode = true;
+  }
+  else {
+    Serial.println("Already in soft AP mode!");
+  }
+}
+
+void waitForConnect() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
+  // Wi-Fi debug info
   Serial.println("Connected!");
   Serial.println("------- WiFi DEBUG ------- ");
   Serial.print("Connected to ");
@@ -89,33 +124,18 @@ void btnHandler() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   WiFi.printDiag(Serial);
-
-
-  server.on("/", handleRoot);
-  server.on("/success", handleSubmit);
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works without need of authentification");
-  });
-
-  server.onNotFound(handleNotFound);
-  //here the list of headers to be recorded
-  const char * headerkeys[] = {"User-Agent", "Cookie"} ;
-  size_t headerkeyssize = sizeof(headerkeys) / sizeof(char*);
-  //ask server to track these headers
-  server.collectHeaders(headerkeys, headerkeyssize );
-  server.begin();
-  Serial.println("HTTP server started");
-
-  apMode = true;
+  Serial.println();
 }
 
 void setup()
 {
   // Set pins and baud rate
   Serial.begin(9600);
+  delay(10);  // Wait for serial port to connect
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(STATUS_LIGHT, OUTPUT);
 
+  Serial.println("\nStart");
   // Initialization and connection to WiFi
   aerServer.init(ssid, password);
   Serial.println("Connected!");
