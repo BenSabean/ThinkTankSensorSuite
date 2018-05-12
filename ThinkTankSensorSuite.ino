07ae85a1b3396ef685171b806ba1406801b5fefc
Benjamin <sabean.ben@gmail.com>
2018-05-12 09:02:10 -0300

    Removed delays.

--- ThinkTankSensorSuite.ino
+++ ThinkTankSensorSuite.ino
@@ -5,33 +5,38 @@
 
 #include <OneWire.h>                // Library for One-Wire interface
 #include <DallasTemperature.h>      // Library for DS18B20 temp. sensor
+#include <EEPROM.h>
 #include <stdio.h>
+#include <ctype.h>
 
-#define DEBUG true
-#define SAMPLE_RATE 5000
+#define BUFFSIZE 80
 #define ADDR_LENGTH 8
 #define DELIM ":"
+
 
 //-----------------Dallas_Library-----------------//
 #define ONE_WIRE_GPIO 2               // Starting data pin of the bus
 #define ONE_WIRE_LENGTH 8             // The number of pins used by the bus
 #define CALLIBRATION 2                // Used to callibrate the sensors (in C)
 volatile uint8_t oneWire_count = 0;
-// Setting up the interface for OneWire communication
-//OneWire oneWire = OneWire(ONE_WIRE_GPIO);
+unsigned long SAMPLE_RATE;             // sample rate in miliseconds
 
+unsigned long start = 0;
+unsigned long now = 0;
+
+// Setting up the interface for OneWire communication
 OneWire oneWire[]  = {
   OneWire(ONE_WIRE_GPIO),
   OneWire(ONE_WIRE_GPIO + 1),
   OneWire(ONE_WIRE_GPIO + 2),
-  OneWire(ONE_WIRE_GPIO + 3), 
+  OneWire(ONE_WIRE_GPIO + 3),
   OneWire(ONE_WIRE_GPIO + 4),
   OneWire(ONE_WIRE_GPIO + 5),
   OneWire(ONE_WIRE_GPIO + 6),
   OneWire(ONE_WIRE_GPIO + 7),
 };
 
-// Creating an instans of DallasTemperature Class with reference to OneWire interface
+// Creating an instance of DallasTemperature Class with reference to OneWire interface
 DallasTemperature Dallas_Library[] = {
   DallasTemperature(&oneWire[0]),
   DallasTemperature(&oneWire[1]),
@@ -48,11 +53,31 @@ struct oneWire_struct {
   float value;
 };
 
-oneWire_struct *pOneWire;
+oneWire_struct* pOneWire;
 
-/*
-   Initialized oneWire sensors + gets the number of sensors
-*/
+//------------------------------------------------------
+// Get a string from EEPROM
+// @param startAddr the starting address of the string
+//            in EEPROM
+// @return char* the current string at the address given
+//            by startAddr
+//-------------------------------------------------------
+char* getString(int startAddr) {
+  char str[BUFFSIZE];
+  memset(str, NULL, BUFFSIZE);
+
+  for (int i = 0; i < BUFFSIZE; i ++) {
+    str[i] = char(EEPROM.read(startAddr + i));
+  }
+  return str;
+}
+
+//------------------------------------------------------
+// Initialized oneWire sensors + gets the number of 
+// sensors
+// @param none
+// @return uint8_t The number of attached sensors
+//-------------------------------------------------------
 uint8_t TempSensors_init () {
   uint8_t _count = 0;
   for (int i = 0; i <  ONE_WIRE_LENGTH; i++) {
@@ -63,15 +88,16 @@ uint8_t TempSensors_init () {
   return _count;
 }
 
-/*
-   Reading one sensor
-   Modifing pointer for one sensor with address and value
-*/
-void TempSensors_getTemp( oneWire_struct **_sensor) {
+//------------------------------------------------------
+// Read values from the OneWire sensors
+// @param oneWire_struct** All of the oneWire_struct 
+//               structures
+// @return void
+//-------------------------------------------------------
+void TempSensors_getTemp( oneWire_struct** _sensor) {
   int index = 0;
-  for (int i = 0; i <= ONE_WIRE_LENGTH; i++) {
-    while (oneWire[i].search((*_sensor + index)->address)) {
-      //Serial.println("index: " + String(index));
+  for (int i = 0; i <= ONE_WIRE_LENGTH; i++) {                // Get a OneWire structure
+    while (oneWire[i].search((*_sensor + index)->address)) {  // Get all values from those sensors
       (*_sensor + index)->value = Dallas_Library[i].getTempC((*_sensor + index)->address);
       (*_sensor + index)->value -= CALLIBRATION;
       index++;
@@ -79,6 +105,13 @@ void TempSensors_getTemp( oneWire_struct **_sensor) {
   }
 }
 
+//------------------------------------------------------
+// Wrapper for TempSensors_getTemp() to read values 
+// from the OneWire sensors
+// @param oneWire_struct[] All of the oneWire_struct 
+//               structures
+// @return void
+//-------------------------------------------------------
 void ReadSensors(oneWire_struct TempSensor[]) {
   // Create a pointer to pass in the full array of structures
   pOneWire = &TempSensor[0];
@@ -87,10 +120,23 @@ void ReadSensors(oneWire_struct TempSensor[]) {
   TempSensors_getTemp(&pOneWire);
 }
 
+//------------------------------------------------------
+// Write all temperature valuse to Serial
+// @param oneWire_struct[] All of the oneWire_struct 
+//               structures
+// @return void
+//-------------------------------------------------------
 void PrintValues(oneWire_struct TempSensor[]) {
+  const unsigned long wait = 500;
+  unsigned long sStart;
   Serial.print("DeviceCount" + String(DELIM) + String(oneWire_count) + "\n");
-  delay(500);
-  // Display current sensor readings and addresses
+  sStart = millis();
+  while ((millis() - sStart) <= wait);
+  Serial.print("SampleRate" + String(DELIM) + String(SAMPLE_RATE) + "\n");
+  sStart = millis();
+  while ((millis() - sStart) <= wait);
+  
+  // Display current sensor readings
   for (uint8_t i = 0; i < oneWire_count; i++) {
     Serial.print("[");
     Serial.print(int(TempSensor[i].address[0]), HEX);
@@ -100,7 +146,8 @@ void PrintValues(oneWire_struct TempSensor[]) {
     }
     Serial.print("]" + String(DELIM));
     Serial.print(String(TempSensor[i].value) + "\n");
-    delay(500);
+    sStart = millis();
+    while ((millis() - sStart) <= wait);
   }
 }
 
@@ -109,8 +156,11 @@ void setup() {
   /* Initializing OneWire Sensors */
   TempSensors_init();
 
-
   Serial.begin(9600);
+  EEPROM.begin();
+  //EEPROM.put(0, SAMPLE_RATE);              // Reset EEPROM
+
+  EEPROM.get(0, SAMPLE_RATE);
 
   // Getting Sensor count
   oneWire_count = TempSensors_init();       // Getting number of sensors
@@ -118,16 +168,25 @@ void setup() {
 
   ReadSensors(TempSensor);
   PrintValues(TempSensor);
+  start = millis();
 }
 
 void loop() {
-  oneWire_count = TempSensors_init();       // Getting number of sensors
-  oneWire_struct TempSensor[oneWire_count]; // structure that holds the sensors
-  ReadSensors(TempSensor);
+  now = millis();
+  if (Serial.available()) {
+    String msg = Serial.readString();
+    if (msg.toInt()) {
+      EEPROM.put(0, msg.toInt());              // Update Sample Rate
+      EEPROM.get(0, SAMPLE_RATE);
+    }
+  }
+  if ((now - start) > SAMPLE_RATE) {
+    oneWire_count = TempSensors_init();       // Getting number of sensors
+    oneWire_struct TempSensor[oneWire_count]; // structure that holds the sensors
+    ReadSensors(TempSensor);
 
-  // Display current sensor readings and addresses
-  PrintValues(TempSensor);
-
-  // Enforce sample rate
-  delay(SAMPLE_RATE);
+    // Display current sensor readings and addresses
+    PrintValues(TempSensor);
+    start = millis();
+  }
 }
